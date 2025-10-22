808babedfdf115cab6382fde1be5f3f38fadbe24
Tomasz Herman <tomekh2015@gmail.com>
2025-10-22 15:52:01 +0200

    Initialize project with base files

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from .models import Task
 
 def home(request):
     return render(request, 'home.html')
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'task_list.html', {'tasks': tasks})
