0ba7fa6a0f537c0df5b1e07e7321241d8ebf33ac
Tomasz Herman <tomekh2015@gmail.com>
2025-10-22 17:35:34 +0200

    Create ModelForms for Position, Worker, TaskType and Task for easy data entry

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Position, Worker, Task
+from .models import Position, Worker, Task, TaskType
 
 
 class PositionForm(forms.ModelForm):
@@ -11,16 +11,28 @@ class PositionForm(forms.ModelForm):
 class WorkerForm(forms.ModelForm):
     class Meta:
         model = Worker
-        fields = ['username', 'first_name', 'last_name', 'email', 'position', 'password']
+        fields = [
+            'username', 'first_name', 'last_name', 'email',
+            'position', 'password'
+        ]
         widgets = {
             'password': forms.PasswordInput(),
         }
 
 
+class TaskTypeForm(forms.ModelForm):
+    class Meta:
+        model = TaskType
+        fields = ['name']
+
+
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['name', 'description', 'deadline', 'priority', 'task_type', 'assignees']
+        fields = [
+            'name', 'description', 'deadline', 'priority',
+            'task_type', 'assignees'
+        ]
         widgets = {
             'deadline': forms.DateInput(attrs={'type': 'date'}),
         }
