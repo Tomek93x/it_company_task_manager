fa37b90450d3c7bc52e7ba6e6d1923d618f1f0d3
Tomasz Herman <tomekh2015@gmail.com>
2025-10-22 17:37:00 +0200

    Setup URL patterns for all main views in IT manager app

--- it_company_task_manager/urls.py
+++ it_company_task_manager/urls.py
@@ -1,7 +1,13 @@
-from django.contrib import admin
-from django.urls import path, include
+"""URL configuration for catalog app."""
+
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('catalog.urls')),
+    path('', views.home, name='home'),
+    path('add-worker/', views.add_worker, name='add_worker'),
+    path('add-position/', views.add_position, name='add_position'),
+    path('add-task-type/', views.add_task_type, name='add_task_type'),
+    path('add-task/', views.add_task, name='add_task'),
+    path('tasks/', views.task_list, name='task_list'),
 ]
